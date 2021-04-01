c5f1bd96787a8defbc51f08c10b8866ab172805f
gbhandari1s <gaurav.bhandari277@gmail.com>
2021-04-01 16:01:10 -0500

    Add new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,18 @@ class ArticlesController < ApplicationController
        @articles = Article.all 
     end
 
+    #creating a new article
+
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+
+        #redirect_to @article ; rails provides this shortcut to us.
+        redirect_to article_path(@article)
+    end
+
 end
