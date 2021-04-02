ef9fe29cf07f43dc6a05b1b86e7b1f138b1973e3
gbhandari1s <gaurav.bhandari277@gmail.com>
2021-04-02 10:24:45 -0500

    Add edit and update routes and actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,20 @@ class ArticlesController < ApplicationController
        @articles = Article.all 
     end
 
-    #creating a new article
+    #creating a new article(new & create), edit an article (edit & update)
 
     def new
 
         @article = Article.new
 
     end
+
+    def edit
+
+        @article = Article.find(params[:id])
+
+    end
+
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -28,5 +35,23 @@ class ArticlesController < ApplicationController
 
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+
+            flash[:notice] = "Article was updated successfully"
+            redirect_to article_path(@article)
+
+        else
+
+            render 'edit'
+
+        end
+
+
+    end
+
+
 
 end
